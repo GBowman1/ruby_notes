9f709c0e02858b10b68e3c0300baa5941b6f2e6d
Garrett Bowman <garrettbowman248@gmail.com>
2024-03-19 11:22:01 -0500

    Feat: ruby classes notes

---
+++ mod_1/objects_classess_instances.rb
@@ -0,0 +1,12 @@
+class Dog
+    def intialize(name, age)
+        @name = name
+        @age = age
+        # set attr for dog
+    end
+
+    def name
+        @name
+    end
+end
+# each class is its own file
---
+++ mod_1/runner.rb
@@ -0,0 +1,7 @@
+require "./objects_classess_instances.rb"
+# import like javascript
+
+dog_1 = Dog.new("Atlas", 3)
+#instantiation
+#instantiate a new Dog instance
+puts Dog.name
